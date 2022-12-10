ced2ef620b051d96c31256aa3f7c4747f4f2b92d
Christopher Brückner <christopher.brueckner95@gmail.com>
2022-12-10 05:10:19 +0100

    feat: use streaming to process large JSON files

--- index_data.py
+++ index_data.py
@@ -1,14 +1,16 @@
-import json
+import re
 import sys
 from datetime import datetime
 from itertools import chain
 
+import ijson
 from elasticsearch import Elasticsearch
 from tqdm import tqdm
 
 
 def chunk_generator(lst, n):
     """Yield successive n-sized chunks from lst.
+
     https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
     """
     for i in range(0, len(lst), n):
@@ -22,18 +24,29 @@ def strip_time(time: datetime) -> str:
 
 if __name__ == "__main__":
     try:
-        chunk_size = sys.argv[2] if len(sys.argv) > 2 else 512
-        with open(sys.argv[1], "r") as json_file:
-            data = json.loads(json_file.read())
-
+        chunk_size = int(sys.argv[2]) if len(sys.argv) > 2 else 512
+        index = re.search(r"\w+$", sys.argv[1][:-12]).string
+        json_file = open(sys.argv[1], "r")
     except:
         print("Usage: python index_data.py <filepath> [<chunksize>]")
         print("Supported files:", end=" ")
         print("bild_export.json faz_export.json spiegel_export.json welt_export.json")
         exit()
 
-    index = list(data.keys())[0]
-    docs = data[index]
+    es = Elasticsearch(
+        "http://localhost:9200",
+        request_timeout=60,
+        retry_on_timeout=True,
+        max_retries=100,
+    )
+
+    docs = []
+
+    for doc in ijson.items(json_file, f"{index}.item"):
+        docs.append(doc)
+
+    json_file.close()
+
     ops = list(
         chain(
             *[
@@ -44,10 +57,8 @@ if __name__ == "__main__":
     )
     chunks = list(chunk_generator(ops, chunk_size * 2))
 
-    es = Elasticsearch("http://localhost:9200")
-
     print(f"Bulk: {len(docs)} documents ({len(chunks)} chunks of size {chunk_size})")
-    print(f"Start: {strip_time(datetime.now)}\n")
+    print(f"Start: {strip_time(datetime.now())}\n")
 
     for chunk in tqdm(chunks):
         es.bulk(operations=chunk)
