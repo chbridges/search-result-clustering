eee3c0336a3ccf2da6c34009c24b9093e01b5bb2
Christopher Brückner <christopher.brueckner95@gmail.com>
2022-12-21 03:48:35 +0100

    feat: add transformer document embeddings

--- search_clustering/embedding.py
+++ search_clustering/embedding.py
@@ -1,8 +1,14 @@
+import multiprocessing
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Union
 
 import numpy as np
 import spacy
+from flair.data import Sentence
+from flair.embeddings import (
+    SentenceTransformerDocumentEmbeddings,
+    TransformerDocumentEmbeddings,
+)
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -13,6 +19,42 @@ class Embedding(ABC):
     @abstractmethod
     def transform(self, docs: List[dict]) -> np.ndarray:
         raise NotImplementedError
+
+
+class TransformerEmbedding(Embedding):
+    """Embed input documents in transformer document embeddings."""
+
+    embedding_model: Union[
+        SentenceTransformerDocumentEmbeddings, TransformerDocumentEmbeddings
+    ]
+
+    def tokenize(self, doc):
+        return Sentence(doc["_source"]["body"])
+
+    def transform(self, docs: List[dict]) -> np.ndarray:
+        pool = multiprocessing.Pool()
+        tokenized_docs = list(pool.map(self.tokenize, docs))
+        self.embedding_model.embed(tokenized_docs)
+        return np.vstack([doc.embedding for doc in tokenized_docs])
+
+
+class DistilBERT(TransformerEmbedding):
+    """Embed input documents in DistilBERT document embeddings and crash my
+    laptop."""
+
+    def __init__(self) -> None:
+        self.embedding_model = TransformerDocumentEmbeddings(
+            "bert-base-german-cased", fine_tune=False
+        )
+
+
+class SentenceMiniLM(TransformerEmbedding):
+    """Embed input documents in sentence MiniLM document embeddings."""
+
+    def __init__(self) -> None:
+        self.embedding_model = SentenceTransformerDocumentEmbeddings(
+            "paraphrase-multilingual-MiniLM-L12-v2"
+        )
 
 
 class Snippet2Vec(Embedding):
@@ -37,7 +79,7 @@ class Nefidf(Embedding):
     """Embed named entities in TF-IDF vectors."""
 
     def transform(self, docs: List[dict]) -> np.ndarray:
-        snippets = [doc["_source"]["introduction"] for doc in docs]
+        snippets = [doc["_source"]["body"] for doc in docs]
         ner = spacy.load("en_core_web_sm")
         entities = [" ".join(map(str, ner(snippet).ents)) for snippet in snippets]
         return TfidfVectorizer().fit_transform(entities).todense()
