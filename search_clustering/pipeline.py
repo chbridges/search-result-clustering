af0a457e7051adeb62aa605b6e896ffc2d01081b
Christopher Brückner <christopher.brueckner95@gmail.com>
2023-01-04 23:08:50 +0100

    feat: add dimensionality reduction as new intermediate step

--- search_clustering/pipeline.py
+++ search_clustering/pipeline.py
@@ -1,14 +1,15 @@
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.lines import Line2D
 from sklearn.decomposition import PCA
+from umap import UMAP
 
 from search_clustering.clustering import Clustering
 from search_clustering.embedding import Embedding
 from search_clustering.labeling import Labeling
 from search_clustering.preprocessing import Preprocessing
+from search_clustering.reduction import Reduction
 
 
 class Pipeline:
@@ -19,31 +20,46 @@ class Pipeline:
         self,
         preprocessing: Preprocessing,
         embedding: Embedding,
+        reductions: Union[Reduction, List[Reduction]],
         clustering: Clustering,
         labeling: Labeling,
     ):
         self.preprocessing = preprocessing
         self.embedding = embedding
+        self.reductions = reductions if isinstance(reductions, list) else [reductions]
         self.clustering = clustering
         self.labeling = labeling
 
     def run(
         self, docs: List[dict], visualize=True
     ) -> Tuple[np.ndarray, np.ndarray, List[str], float]:
+        steps = 5 + visualize
+
+        print(f"[1/{steps}] Preprocessing")
         docs = self.preprocessing.transform(docs)
+
+        print(f"[2/{steps}] Embedding")
         vecs = self.embedding.transform(docs)
+
+        print(f"[3/{steps}] Reducing Dimensionality")
+        for reduction in self.reductions:
+            vecs = reduction.transform(vecs)
+
+        print(f"[4/{steps}] Clustering")
         clusters, score = self.clustering.fit_predict(vecs)
+
+        print(f"[5/{steps}] Labeling")
         labels = self.labeling.fit_predict(docs, clusters)
 
         if visualize:
+            print(f"[6/{steps}] Visualizing")
             self.visualize(vecs, clusters, labels)
-            for label in labels:
-                print(label)
 
         return vecs, clusters, labels, score
 
-    def visualize(self, vecs, clusters, labels):
-        pca = PCA(n_components=2).fit_transform(vecs)
+    @staticmethod
+    def visualize(vecs, clusters, labels):
+        pca = UMAP(n_components=2).fit_transform(vecs)
         colors = [f"C{i}" for i in range(len(labels))]
         counts = [len(clusters[clusters == c]) for c in sorted(set(clusters))]
         if -1 in clusters:
@@ -59,7 +75,7 @@ class Pipeline:
                 linestyle="None",
                 color=colors[i],
                 marker="o",
-                label=f"{counts[i]}",
+                label=f"{labels[i]}",
             )
             for i in range(len(labels))
         ]
---
+++ search_clustering/reduction.py
@@ -0,0 +1,46 @@
+from math import log
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+import sklearn.decomposition
+import umap
+
+
+class Reduction(ABC):
+    """Reduce dimensionality of word embeddings."""
+
+    @abstractmethod
+    def transform(self, vecs: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+
+
+class PCA(Reduction):
+    """Perform Principal Component Analysis on word embeddings."""
+    def __init__(self, n_components: int = 64) -> None:
+        self.n_components = n_components
+
+    def transform(self, vecs: np.ndarray) -> np.ndarray:
+        pca = sklearn.decomposition.PCA(n_components=min(len(vecs), self.n_components))
+        return pca.fit_transform(vecs)
+
+class UMAP(Reduction):
+    """Perform Principal Component Analysis on word embeddings."""
+    def __init__(self, n_components: int = 8) -> None:
+        self.n_components = n_components
+        self.densmap = False
+
+    def transform(self, vecs: np.ndarray) -> np.ndarray:
+        return umap.UMAP(
+            densmap=self.densmap,
+            n_neighbors=round(2 * log(len(vecs), 2)),
+            min_dist=0.0,
+            n_components=max(self.n_components, 2),
+            random_state=42,
+        ).fit_transform(vecs)
+
+class DensMAP(UMAP):
+    """Perform Principal Component Analysis on word embeddings."""
+    def __init__(self, n_components: int = 8) -> None:
+        super().__init__(n_components)
+        self.densmap = True
